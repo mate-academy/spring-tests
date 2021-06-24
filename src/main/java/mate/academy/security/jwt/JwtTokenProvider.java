b16104eeef402d8429da008d1b6ccba73d4751f7
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-06-24 12:14:42 +0300

    removed TODO

--- src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -70,7 +70,7 @@ public class JwtTokenProvider {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            throw new RuntimeException("Expired or invalid JWT token", e); //TODO: create custom exception
+            throw new RuntimeException("Expired or invalid JWT token", e);
         }
     }
 }
