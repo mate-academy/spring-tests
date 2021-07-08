5d6407fd3005b04f96a08ffb52f5704197212d7a
Nikber5 <nikber198@gmail.com>
2021-07-08 17:53:56 +0300

    removed static imports

--- src/main/test/mate/academy/validation/PasswordValidatorTest.java
+++ src/main/test/mate/academy/validation/PasswordValidatorTest.java
@@ -1,7 +1,5 @@
 package mate.academy.validation;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import javax.validation.ConstraintValidatorContext;
 import mate.academy.model.dto.UserRegistrationDto;
 import org.junit.jupiter.api.Assertions;
