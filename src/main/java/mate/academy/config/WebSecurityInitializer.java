43ebeec4c1dbaa20b8fa577a859a81fb7c3472ff
Andriy_Poluhovic <chimbar@ukr.net>
2022-04-19 21:02:40 +0300

    Was added tests to dap package

--- src/main/java/mate/academy/config/WebSecurityInitializer.java
+++ src/main/java/mate/academy/config/WebSecurityInitializer.java
@@ -3,5 +3,4 @@ package mate.academy.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
 }
