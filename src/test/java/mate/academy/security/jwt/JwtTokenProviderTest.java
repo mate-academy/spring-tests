acf1d955c4370dfe96e37c117796a8325bfbbbfd
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-05-04 14:55:19 +0300

    Add JwtTokenProviderTest

--- src/test/java/mate/academy/security/jwt/JwtTokenProviderTest.java
+++ src/test/java/mate/academy/security/jwt/JwtTokenProviderTest.java
@@ -2,19 +2,27 @@ package mate.academy.security.jwt;
 
 import mate.academy.model.Role;
 import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 class JwtTokenProviderTest {
+    private final static String CORRECT_TOKEN = "eyJhbGciOiJIUzI1NiJ9"
+            + ".eyJzdWIiOiJib2JAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiJdLCJpYXQ"
+            + "iOjE2NTE2NjMwNTcsImV4cCI6OTQ0NTEwMjQyNjU3fQ"
+            + ".mk_l0poZNHTAqbunh6BQ22KQfoIowdbs4MsfR9ixKMw";
+    private final static String INCORRECT_TOKEN = "11111111111"
+            + ".2222222222222"
+            + ".3333333333333";
+    private final static String EMAIL = "bob@gmail.com";
+    private final static String PASSWORD = "12345678";
     private JwtTokenProvider jwtTokenProvider;
     private UserDetailsService userDetailsService;
     @BeforeEach
@@ -25,10 +33,12 @@ class JwtTokenProviderTest {
             jwtTokenProvider = jwtTokenProviderClass
                     .getConstructor(UserDetailsService.class)
                     .newInstance(userDetailsService);
+
             Field secretKeyFiled = jwtTokenProviderClass
-                    .getField("secretKey");
+                    .getDeclaredField("secretKey");
+
             Field validityInMilliseconds = jwtTokenProviderClass
-                    .getField("validityInMilliseconds");
+                    .getDeclaredField("validityInMilliseconds");
             secretKeyFiled.setAccessible(true);
             validityInMilliseconds.setAccessible(true);
             secretKeyFiled.set(jwtTokenProvider, "secret");
@@ -41,26 +51,56 @@ class JwtTokenProviderTest {
 
     @Test
     void createToken_Ok() {
-        String token = jwtTokenProvider.createToken("bob@gmail.com",
+        String token = jwtTokenProvider.createToken(EMAIL,
                 List.of(Role.RoleName.USER.name()));
         Assertions.assertNotNull(token);
-        Assertions.assertFalse(token.isEmpty());
-        Assertions.assertEquals(3, token.split(".").length);
+        Assertions.assertEquals(3, token.split("\\.").length);
     }
 
     @Test
-    void getAuthentication() {
+    void validateToken_Ok() {
+        boolean actual = jwtTokenProvider.validateToken(CORRECT_TOKEN);
+        Assertions.assertTrue(actual);
     }
 
     @Test
-    void getUsername() {
+    void validateToken_NotOk() {
+        try {
+            boolean actual = jwtTokenProvider.validateToken(INCORRECT_TOKEN);
+            Assertions.fail("Expected to receive Exception");
+        } catch (Exception e) {
+            Assertions.assertEquals("Expired or invalid JWT token",
+                    e.getMessage());
+        }
     }
 
     @Test
-    void resolveToken() {
+    void getUsername_Ok() {
+        String actual = jwtTokenProvider.getUsername(CORRECT_TOKEN);
+        Assertions.assertEquals(EMAIL, actual);
     }
 
     @Test
-    void validateToken() {
+    void getAuthentication_Ok() {
+        User.UserBuilder userBuilder = User.withUsername(EMAIL);
+        userBuilder.password(PASSWORD);
+        userBuilder.roles(Role.RoleName.USER.name());
+        Mockito.when(userDetailsService.loadUserByUsername(EMAIL))
+                .thenReturn(userBuilder.build());
+        Authentication actual = jwtTokenProvider.getAuthentication(CORRECT_TOKEN);
+        Assertions.assertEquals(EMAIL, actual.getName());
+        Assertions.assertTrue(actual.getAuthorities()
+                .stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_" + Role.RoleName.USER.name())));
     }
+
+
+    @Test
+    void resolveTokenOk() {
+        HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+        Mockito.when(req.getHeader("Authorization")).thenReturn("Bearer " + CORRECT_TOKEN);
+        String actual = jwtTokenProvider.resolveToken(req);
+        Assertions.assertEquals(CORRECT_TOKEN, actual);
+    }
+
 }
