b5c9ed1567fa14a73f71a35f134e8379ae0bbe7d
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-05-04 23:53:52 +0300

    Add RoleServiceImplTest

--- src/test/java/mate/academy/util/UserTestUtil.java
+++ src/test/java/mate/academy/util/UserTestUtil.java
@@ -27,7 +27,7 @@ public class UserTestUtil {
         user.setRoles(Set.of(userRole));
         return user;
     }
-    
+
     public static Role getUserRole() {
         Role expected = new Role();
         expected.setRoleName(Role.RoleName.USER);
