8e0a061550fb7c7e04d0828a2bc8e14969c085dd
ssplot56 <ssplot56@gmail.com>
2022-10-04 01:38:37 +0300

    tests are created

---
+++ src/test/java/mate/academy/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,50 @@
+package mate.academy.security;
+
+import java.util.Optional;
+import java.util.Set;
+import mate.academy.model.Role;
+import mate.academy.model.User;
+import mate.academy.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+class CustomUserDetailsServiceTest {
+    private static final String EMAIL = "bob@mail.com";
+    private static final String PASSWORD = "bobNumber1";
+    private CustomUserDetailsService customUserDetailsService;
+
+    @BeforeEach
+    void setUp() {
+        UserService userService = Mockito.mock(UserService.class);
+        customUserDetailsService = new CustomUserDetailsService(userService);
+
+        User user = new User();
+        user.setEmail(EMAIL);
+        user.setPassword(PASSWORD);
+        user.setRoles(Set.of(new Role(Role.RoleName.USER)));
+        Mockito.when(userService.findByEmail(EMAIL)).thenReturn(Optional.of(user));
+    }
+
+    @Test
+    void loadUserByUsername_Ok() {
+        UserDetails actual = customUserDetailsService.loadUserByUsername(EMAIL);
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(EMAIL, actual.getUsername());
+        Assertions.assertEquals(PASSWORD, actual.getPassword());
+    }
+
+    @Test
+    void loadUserByUsername_UsernameNotFoundException() {
+        try {
+            customUserDetailsService.loadUserByUsername("alice@mail.com");
+        } catch (UsernameNotFoundException e) {
+            Assertions.assertEquals("User not found.", e.getMessage());
+            return;
+        }
+        Assertions.fail("Expected to receive UsernameNotFoundException");
+    }
+}
