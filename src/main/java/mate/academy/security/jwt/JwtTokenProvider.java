0409f8fbc308a17dffa097003361fee3324f7748
Nazar <litvik.12@gmail.com>
2022-10-15 03:48:42 +0300

    added test for JwtTokenProvider

--- src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
+++ src/main/java/mate/academy/security/jwt/JwtTokenProvider.java
@@ -22,7 +22,7 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key:secret}")
     private String secretKey;
     @Value("${security.jwt.token.expire-length:3600000}")
-    private long validityInMilliseconds;
+    private Long validityInMilliseconds;
     private final UserDetailsService userDetailsService;
 
     public JwtTokenProvider(UserDetailsService userDetailsService) {
