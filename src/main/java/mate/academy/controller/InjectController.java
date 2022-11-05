cdea85f5024cd1b8ab3605d85468d61a4593fd8b
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-11-06 01:33:47 +0200

    Add Admin user in the injectData() method of InjectController

--- src/main/java/mate/academy/controller/InjectController.java
+++ src/main/java/mate/academy/controller/InjectController.java
@@ -1,8 +1,11 @@
 package mate.academy.controller;
 
+import java.util.Set;
 import mate.academy.model.Role;
+import mate.academy.model.User;
 import mate.academy.security.AuthenticationService;
 import mate.academy.service.RoleService;
+import mate.academy.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/inject")
 public class InjectController {
     private final RoleService roleService;
-
+    private final UserService userService;
     private final AuthenticationService authenticationService;
 
-    public InjectController(RoleService roleService,
+    public InjectController(RoleService roleService, UserService userService,
                             AuthenticationService authenticationService) {
         this.roleService = roleService;
+        this.userService = userService;
         this.authenticationService = authenticationService;
     }
 
@@ -24,10 +28,12 @@ public class InjectController {
     public String injectData() {
         roleService.save(new Role(Role.RoleName.ADMIN));
         roleService.save(new Role(Role.RoleName.USER));
-
         authenticationService.register("bob", "1234");
-        // feel free to save some users for testing
-        // hint: you can save users with different roles
+        User alice = new User();
+        alice.setEmail("alice");
+        alice.setPassword("1234");
+        alice.setRoles(Set.of(roleService.getRoleByName("ADMIN")));
+        userService.save(alice);
         return "Done!";
     }
 }
