bfac7b774d59cda9f4a1e77dd3d307f531c3495f
Roman Voitalik <voitalik1@gmail.com>
2022-12-11 21:52:40 +0200

    complete UserServiceImplTest

--- src/test/java/mate/academy/service/UserServiceImplTest.java
+++ src/test/java/mate/academy/service/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package mate.academy.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
 import java.util.Set;
 import mate.academy.dao.UserDao;
 import mate.academy.model.Role;
@@ -10,7 +11,6 @@ import mate.academy.model.User;
 import mate.academy.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,10 +39,44 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findById() {
+    void findById_userIdExist_Ok() {
+        String password = "12345678";
+        User user = new User();
+        user.setEmail("bob@i.ua");
+        user.setPassword(password);
+        user.setRoles(Set.of(new Role(Role.RoleName.USER), new Role(Role.RoleName.ADMIN)));
+        Mockito.when(userDao.findById(1L)).thenReturn(Optional.of(user));
+        Optional<User> userOptional = userService.findById(1L);
+        assertTrue(userOptional.isPresent());
+        assertEquals(user, userOptional.get());
     }
 
     @Test
-    void findByEmail() {
+    void findById_userIdNotExist_Ok() {
+        Mockito.when(userDao.findById(1L)).thenReturn(Optional.empty());
+        Optional<User> userOptional = userService.findById(1L);
+        assertTrue(userOptional.isEmpty());
+    }
+
+    @Test
+    void findByEmail_emailExist_Ok() {
+        String password = "12345678";
+        String email = "bob@i.ua";
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRoles(Set.of(new Role(Role.RoleName.USER), new Role(Role.RoleName.ADMIN)));
+        Mockito.when(userDao.findByEmail(email)).thenReturn(Optional.of(user));
+        Optional<User> userOptional = userService.findByEmail(email);
+        assertTrue(userOptional.isPresent());
+        assertEquals(user, userOptional.get());
+    }
+
+    @Test
+    void findByEmail_emailNotExist_Ok() {
+        String email = "bob@i.ua";
+        Mockito.when(userDao.findByEmail(email)).thenReturn(Optional.empty());
+        Optional<User> userOptional = userService.findByEmail(email);
+        assertTrue(userOptional.isEmpty());
     }
 }
