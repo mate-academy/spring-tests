2cb038d7738fb26018c30c66e921d4c596858aae
Roman Voitalik <voitalik1@gmail.com>
2022-12-12 00:10:11 +0200

    add RoleDaoImplTest, inside thist class create RoleDaoImplWrappper to extend RoleDaoImpl

---
+++ src/test/java/mate/academy/dao/RoleDaoImplTest.java
@@ -0,0 +1,58 @@
+package mate.academy.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import mate.academy.dao.impl.RoleDaoImpl;
+import mate.academy.model.Role;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RoleDaoImplTest extends AbstractTest{
+
+    private RoleDao roleDao;
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {Role.class};
+    }
+
+    @BeforeEach
+    void setUp() {
+        roleDao = new RoleDaoImplWrappper(getSessionFactory());
+    }
+
+    @Test
+    void save_Ok() {
+        Role roleUser = new Role(Role.RoleName.USER);
+        Role actual = roleDao.save(roleUser);
+        assertNotNull(actual);
+        assertEquals(1L, actual.getId());
+    }
+
+    @Test
+    void getRoleByName_roleExists_Ok() {
+        Role roleUser = new Role(Role.RoleName.USER);
+        roleDao.save(roleUser);
+        Optional<Role> actualRole = roleDao.getRoleByName("USER");
+        assertTrue(actualRole.isPresent());
+        assertEquals(1L, actualRole.get().getId());
+    }
+
+    @Test
+    void getRoleByName_roleNotExist_Ok() {
+        Role roleUser = new Role(Role.RoleName.USER);
+        roleDao.save(roleUser);
+        Optional<Role> actualRole = roleDao.getRoleByName("ADMIN");
+        assertTrue(actualRole.isEmpty());
+    }
+
+    class RoleDaoImplWrappper extends RoleDaoImpl {
+        RoleDaoImplWrappper(SessionFactory sessionFactory) {
+            super(sessionFactory);
+        }
+    }
+}
