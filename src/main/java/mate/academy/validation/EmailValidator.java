23f647db6aede67b5f74b3f7a2684c357e47785f
Roman Voitalik <voitalik1@gmail.com>
2022-12-11 16:43:24 +0200

    add EmailValidatorTest, PasswordValidatorTest, RoleServiceImplTest

--- src/main/java/mate/academy/validation/EmailValidator.java
+++ src/main/java/mate/academy/validation/EmailValidator.java
@@ -6,8 +6,8 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}"
-            + "\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(([0-9]{1,3}"
+            + "\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
---
+++ src/test/java/mate/academy/service/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package mate.academy.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
