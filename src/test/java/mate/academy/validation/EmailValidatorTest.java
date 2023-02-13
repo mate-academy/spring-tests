986d0f175cc1d0ab6dd5136352b06328687a4601
AndriySichniy <sichniy.andriy@gmail.com>
2023-02-13 10:30:58 +0200

    add EmailValidatorTest

---
+++ src/test/java/mate/academy/validation/EmailValidatorTest.java
@@ -0,0 +1,36 @@
+package mate.academy.validation;
+
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.validation.ConstraintValidatorContext;
+
+class EmailValidatorTest {
+    private EmailValidator emailValidator;
+    private ConstraintValidatorContext constraintValidatorContext;
+    private final String WRIGHT_EMAIL = "bobik@g.com";
+    private final String WRONG_EMAIL = "bobik.com";
+
+    @BeforeEach
+    void setup() {
+        emailValidator = new EmailValidator();
+        constraintValidatorContext = Mockito.mock(ConstraintValidatorContext.class);
+    }
+
+    @Test
+    void isValid_Ok() {
+        Assertions.assertTrue(emailValidator.isValid(WRIGHT_EMAIL, constraintValidatorContext));
+    }
+    @Test
+    void isValid_NotOk() {
+        Assertions.assertFalse(emailValidator.isValid(WRONG_EMAIL, constraintValidatorContext));
+    }
+
+    @Test
+    void isValid_NullEmail_NotOk() {
+        Assertions.assertFalse(emailValidator.isValid(null, constraintValidatorContext));
+    }
+}
