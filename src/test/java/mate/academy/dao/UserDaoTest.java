e9fc12da590ac0a3f3546a43efdd5a2837a69d0c
DooMLeoN <levberezov01@gmail.com>
2023-02-24 14:22:54 +0200

    add User test fixes

--- src/test/java/mate/academy/dao/UserDaoTest.java
+++ src/test/java/mate/academy/dao/UserDaoTest.java
@@ -19,15 +19,23 @@ class UserDaoTest extends AbstractTest {
     private static final String ELLIS_PASSWORD = "123";
     private static final Long CHECK_ID = 1L;
     private UserDao userDao;
-    private Role roleUser;
-
+    private User bob;
+    private User elis;
     @BeforeEach
     void setUp() {
         userDao = new UserDaoImpl(getSessionFactory());
-        roleUser = new Role();
+        Role roleUser = new Role();
         roleUser.setRoleName(Role.RoleName.USER);
         RoleDao roleDao = new RoleDaoImpl(getSessionFactory());
         roleDao.save(roleUser);
+        bob = new User();
+        bob.setEmail(BOB_EMAIL);
+        bob.setPassword(BOB_PASSWORD);
+        bob.setRoles(Set.of(roleUser));
+        elis = new User();
+        elis.setEmail(ELLIS_EMAIL);
+        elis.setPassword(ELLIS_PASSWORD);
+        elis.setRoles(Set.of(roleUser));
     }
 
     @Override
@@ -38,10 +46,6 @@ class UserDaoTest extends AbstractTest {
 
     @Test
     void saveUser_Ok() {
-        User bob = new User();
-        bob.setEmail(BOB_EMAIL);
-        bob.setPassword(BOB_PASSWORD);
-
         User actual = userDao.save(bob);
         Assertions.assertNotNull(actual);
         Assertions.assertEquals(CHECK_ID, actual.getId());
@@ -49,34 +53,15 @@ class UserDaoTest extends AbstractTest {
 
     @Test
     void findByEmail_Ok() {
-        User bob = new User();
-        bob.setEmail(BOB_EMAIL);
-        bob.setPassword(BOB_PASSWORD);
-        bob.setRoles(Set.of(roleUser));
-
-        User elis = new User();
-        elis.setEmail(ELLIS_EMAIL);
-        elis.setPassword(ELLIS_PASSWORD);
-        elis.setRoles(Set.of(roleUser));
-
         userDao.save(elis);
         userDao.save(bob);
-        Optional<User> actual = userDao.findByEmail(BOB_EMAIL);
+        Optional<User> actual = userDao.findByEmail(ELLIS_EMAIL);
         Assertions.assertTrue(actual.isPresent());
-        Assertions.assertEquals(bob.getEmail(), actual.get().getEmail());
+        Assertions.assertEquals(elis.getEmail(), actual.get().getEmail());
     }
 
     @Test
     void findById_Ok() {
-        User bob = new User();
-        bob.setEmail(BOB_EMAIL);
-        bob.setPassword(BOB_PASSWORD);
-        bob.setRoles(Set.of(roleUser));
-
-        User elis = new User();
-        elis.setEmail(ELLIS_EMAIL);
-        elis.setPassword(ELLIS_PASSWORD);
-        elis.setRoles(Set.of(roleUser));
         userDao.save(elis);
         userDao.save(bob);
         Optional<User> actual = userDao.findById(CHECK_ID);
