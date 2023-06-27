5d59bc9bb0e8c64367b569cfc55525d256cbc7d4
Skrudj <ssinilov@gmail.com>
2023-06-27 20:07:38 +0300

    create test files and implement AbstractTest

---
+++ src/test/java/mate/academy/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,7 @@
+package mate.academy.dao.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoImplTest {
+
+}
---
+++ src/test/java/mate/academy/security/AuthenticationServiceImplTest.java
@@ -0,0 +1,7 @@
+package mate.academy.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationServiceImplTest {
+
+}
