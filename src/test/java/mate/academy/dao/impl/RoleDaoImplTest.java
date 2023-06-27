65ecce92bb2e4ae215bce9e3105f123c6de9e847
Skrudj <ssinilov@gmail.com>
2023-06-27 20:40:52 +0300

    implement dao tests

--- src/test/java/mate/academy/dao/impl/RoleDaoImplTest.java
+++ src/test/java/mate/academy/dao/impl/RoleDaoImplTest.java
@@ -1,7 +1,54 @@
 package mate.academy.dao.impl;
 
+import java.util.Optional;
+import mate.academy.dao.AbstractTest;
+import mate.academy.dao.RoleDao;
+import mate.academy.model.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RoleDaoImplTest {
+class RoleDaoImplTest extends AbstractTest {
+    private static final Long ID = 1L;
+    private static final Role.RoleName USER_ROLE = Role.RoleName.USER;
+    private static final Role.RoleName ADMIN_ROLE = Role.RoleName.ADMIN;
+    private RoleDao roleDao;
+    private Role role;
 
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {Role.class};
+    }
+
+    @BeforeEach
+    void setUp() {
+        roleDao = new RoleDaoImpl(getSessionFactory());
+        role = new Role();
+        role.setRoleName(Role.RoleName.USER);
+    }
+
+    @Test
+    void save_ok() {
+        Role actual = roleDao.save(role);
+        assertNotNull(actual);
+        assertEquals(ID, actual.getId());
+    }
+
+    @Test
+    void getRoleByName_Ok() {
+        roleDao.save(role);
+        Optional<Role> actual =
+                roleDao.getRoleByName(USER_ROLE.name());
+        assertTrue(actual.isPresent());
+        assertEquals(actual.get().getRoleName(), USER_ROLE);
+    }
+
+    @Test
+    void getRoleByName_InvalidName_NotOk() {
+        roleDao.save(role);
+        Optional<Role> actual =
+                roleDao.getRoleByName(ADMIN_ROLE.name());
+        assertFalse(actual.isPresent());
+    }
 }
--- src/test/java/mate/academy/dao/impl/UserDaoImplTest.java
+++ src/test/java/mate/academy/dao/impl/UserDaoImplTest.java
@@ -1,7 +1,80 @@
 package mate.academy.dao.impl;
 
+import java.util.Optional;
+import java.util.Set;
+import mate.academy.dao.AbstractTest;
+import mate.academy.dao.RoleDao;
+import mate.academy.dao.UserDao;
+import mate.academy.model.Role;
+import mate.academy.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserDaoImplTest {
+class UserDaoImplTest extends AbstractTest {
+    private static final String EMAIL = "bob@i.ua";
+    private static final String INVALID_EMAIL = "alice@i.ua";
+    private static final String PASSWORD = "1234";
+    private static final Long ID = 1L;
+    private static final Long INVALID_ID = 2L;
+    private UserDao userDao;
+    private RoleDao roleDao;
+    private User user;
+    private Role role;
 
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {User.class, Role.class};
+    }
+
+    @BeforeEach
+    void setUp() {
+        userDao = new UserDaoImpl(getSessionFactory());
+        roleDao = new RoleDaoImpl(getSessionFactory());
+        role = new Role();
+        role.setRoleName(Role.RoleName.USER);
+        roleDao.save(role);
+        user = new User();
+        user.setEmail(EMAIL);
+        user.setPassword(PASSWORD);
+        user.setRoles(Set.of(role));
+    }
+
+    @Test
+    void save_Ok() {
+        User actual = userDao.save(user);
+        assertNotNull(actual);
+        assertEquals(1L, actual.getId());
+    }
+
+    @Test
+    void findByEmail_Ok() {
+        userDao.save(user);
+        Optional<User> actual = userDao.findByEmail(EMAIL);
+        assertTrue(actual.isPresent());
+        assertEquals(EMAIL, actual.get().getEmail());
+    }
+
+    @Test
+    void findByEmail_InvalidEmail_NotOk() {
+        userDao.save(user);
+        Optional<User> actual = userDao.findByEmail(INVALID_EMAIL);
+        assertFalse(actual.isPresent());
+    }
+
+    @Test
+    void findById_Ok() {
+        userDao.save(user);
+        Optional<User> actual = userDao.findById(ID);
+        assertTrue(actual.isPresent());
+        assertEquals(ID, actual.get().getId());
+    }
+
+    @Test
+    void findById_InvalidId_NotOk() {
+        userDao.save(user);
+        Optional<User> actual = userDao.findById(INVALID_ID);
+        assertFalse(actual.isPresent());
+    }
 }
