46f74adec05d0e5c6a709bbf54bac5d2151857bc
Sergey gordeichuk <ya.gordeichuk@gmail.com>
2023-07-05 00:09:03 +0300

    Created RoleServiceImplTest and UserServiceImplTest.

---
+++ src/test/java/mate/academy/service/impl/RoleServiceImplTest.java
@@ -0,0 +1,48 @@
+package mate.academy.service.impl;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import mate.academy.dao.RoleDao;
+import mate.academy.model.Role;
+import mate.academy.service.RoleService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class RoleServiceImplTest {
+    private Role role;
+    private RoleDao roleDao;
+    private RoleService roleService;
+
+    @BeforeEach
+    void setUp() {
+        roleDao = Mockito.mock(RoleDao.class);
+        roleService = new RoleServiceImpl(roleDao);
+        role = new Role();
+        role.setRoleName(Role.RoleName.USER);
+    }
+
+    @Test
+    void save_Ok() {
+        Mockito.when(roleDao.save(role)).thenReturn(role);
+        Role actual = roleService.save(role);
+        Assertions.assertNotNull(role);
+        Assertions.assertEquals(role,actual);
+    }
+
+    @Test
+    void getRoleByName_Ok() {
+        Mockito.when(roleDao.getRoleByName(role.getRoleName().name()))
+                .thenReturn(Optional.of(role));
+        Role actual = roleService.getRoleByName(role.getRoleName().name());
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(role, actual);
+    }
+
+    @Test
+    void getRoByName_NotOk() {
+        Assertions.assertThrows(NoSuchElementException.class, () ->
+                roleService.getRoleByName(null));
+    }
+}
